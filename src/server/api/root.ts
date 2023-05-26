53b14e5d73d5a531096e4e5840678ba155e1a797
jeff-gitcode <jeff.gitcode@gmail.com>
2023-05-27 00:06:50 +1000

    updated

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleRouter } from "~/server/api/routers/example";
+import { customerRouter } from "../routes/customer.routes";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { exampleRouter } from "~/server/api/routers/example";
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
+  customer: customerRouter
 });
 
 // export type definition of API
